4a2e6af9515a34a13c07ddaa36f04818d63e8ac8
shining-stars-lk <1031900093@qq.com>
2022-01-21 14:24:37 +0800

    事务线程

---
+++ src/main/java/com/example/transactionthread/TransactionthreadApplication.java
@@ -0,0 +1,13 @@
+package com.example.transactionthread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionthreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionthreadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/transactionthread/TransactionthreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transactionthread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionthreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
